fc99b48bf662f0f917e1d29d3f2e07f6f9ca7d9f
Zalu <victor.ardila0205@gmail.com>
2023-04-20 20:28:05 -0500

    added JS code to the calculator

---
+++ MiWeb/mybriefcase.js
@@ -0,0 +1,10 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperador = document.querySelectorAll('.operador');
+
+const calculadora = new Calculadora();
+console.log(calculadora.sumar(2,3))
+console.log(calculadora.restar(2,3))
+console.log(calculadora.multiplicar(2,3))
+console.log(calculadora.dividir(2,3))
