65917f9a5c05defee25889f6c6837db1c8693684
Victor <victor.ardila0205@gmail.com>
2024-09-10 00:01:29 -0500

    new reactive design

--- imgScript.js
+++ imgScript.js
@@ -4,13 +4,13 @@ const img = document.getElementById("myPhoto"),
 window.onload = function() {
 
     setTimeout(() => {
-      img.style.width = '16vw';
+      img.style.width = '18vw';
     }, 180);
     setTimeout(() => {
-      secretDiv.style.width = '20vh'
-      secretDiv.style.height = '20vh'
+      secretDiv.style.width = '24vh'
+      secretDiv.style.height = '24vh'
       secretDiv.style.color = '#ffffffde'
-      secretDiv.style.backgroundColor = '#00000047'
+      secretDiv.style.backgroundColor = '#00000070'
     }, 1500);
     setTimeout(() => {
       img.style.transform = 'rotate(540deg)';
