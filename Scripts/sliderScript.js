6c657eddb6532a3244455811e7a0c9474a849285
Victor <victor.ardila0205@gmail.com>
2024-09-12 23:18:24 -0500

    edited buttons Scripts

--- Scripts/sliderScript.js
+++ Scripts/sliderScript.js
@@ -4,17 +4,30 @@ const
     slider = document.querySelector("#containerSec"),
     slidersec = document.querySelectorAll(".slidersel");
 
+    window.onload = function() {
+        btnL.style.backgroundColor = '#fff';
+      };
+
 btnL.addEventListener("click",e=> moveToLeft());
 btnR.addEventListener("click",e=> moveToRight());
 
 function moveToRight() {
     slidersec.forEach(slide => {
         slide.style.transform = "translateX(-100%)";
+        if (slide.style.transform === "translateX(-100%)") {
+            btnR.style.backgroundColor = '#fff';
+            btnL.style.backgroundColor = '';
+        }
     });
 }
+
 
 function moveToLeft() {
     slidersec.forEach(slide => {
         slide.style.transform = "translateX(0%)";
+        if (slide.style.transform === "translateX(0%)") {
+            btnL.style.backgroundColor = '#fff';
+            btnR.style.backgroundColor = '';
+        }
     });
 }
