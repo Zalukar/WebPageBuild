3c2a36296aeaf2a99720f902e1b7db200b3fa772
Victor <victor.ardila0205@gmail.com>
2024-09-19 02:45:36 -0500

    Renamed some stuff on JavaScript contactBtns

--- Scripts/contactBtns.js
+++ Scripts/contactBtns.js
@@ -1,21 +1,21 @@
 const 
-    linkedin = document.getElementById("stackOverflow"),
+    curriculum = document.getElementById("stackOverflow"),
     gitHub = document.getElementById("gitHub"),
     fontColorStack = document.getElementById("spanStack"),
     fontColorGit = document.getElementById("spanGit"),
     imgGit = document.getElementById("imgGit"),
     imgLinked = document.getElementById("imgLinked");
 
-linkedin.addEventListener("mouseover", () => {
-        linkedin.style.transform = "scale(1.15)";
-        linkedin.style.backgroundColor = "#1a6b23db";
-        linkedin.style.cursor = 'pointer'
+curriculum.addEventListener("mouseover", () => {
+        curriculum.style.transform = "scale(1.15)";
+        curriculum.style.backgroundColor = "#1a6b23db";
+        curriculum.style.cursor = 'pointer'
         fontColorGit.style.color = "#D9D9D9";
     });
     
-linkedin.addEventListener("mouseout", () => {
-        linkedin.style.transform = "scale(1)";
-        linkedin.style.backgroundColor = "#D9D9D9";
+curriculum.addEventListener("mouseout", () => {
+        curriculum.style.transform = "scale(1)";
+        curriculum.style.backgroundColor = "#D9D9D9";
         fontColorGit.style.color = "#000";
     });
     
@@ -35,7 +35,7 @@ gitHub.addEventListener("mouseout", () => {
 
     });
 
-linkedin.addEventListener('click',function(){
+curriculum.addEventListener('click',function(){
     window.location.href = 'https://stackoverflow.com/users/20536708/manuel-ardila';
     
 })
